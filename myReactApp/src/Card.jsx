0104a575e665b70f9a44769c6051d4121482f741
Ameenul Firdous <127436359+d3ku010@users.noreply.github.com>
2025-03-28 16:23:57 +0530

    0.1.2

--- myReactApp/src/Card.jsx
+++ myReactApp/src/Card.jsx
@@ -1,4 +1,5 @@
 import ProfilePic from './assets/profile.jpg'
+import Button from './Button/Button'
 
 function Card() {
 
@@ -7,6 +8,7 @@ function Card() {
             <img className="card-image" src={ProfilePic} alt="profile picture"></img>
             <h2 className="card-title" >Pathu</h2>
             <p className="card-text" >I am Pathumma</p>
+            <Button />
         </div >
     );
 }
