0a662985f92d1dcd50c2eb7fed6cdc790241c142
Ameenul Firdous <127436359+d3ku010@users.noreply.github.com>
2025-05-23 18:01:21 +0530

    0.3

--- Feedback App/feedback-app/src/App.js
+++ Feedback App/feedback-app/src/App.js
@@ -1,4 +1,5 @@
 
+import FeedbackStats from './components/FeedbackStats'
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
@@ -7,11 +8,19 @@ import FeedbackData from './data/FeedbackData'
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
   return (
     <>
       <Header />
       < div className='container'>
-        <FeedbackList feedback={feedback} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback}
+          handleDelete={deleteFeedback} />
       </div >
     </>
   )
